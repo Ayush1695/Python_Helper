800c33ed29659d5b670ac39140c7b9e352f7db19
AYUSH1695 <ayushoturkar@gmail.com>
2022-05-04 00:23:56 +0530

    Adding new file with latest logics and python ideas

---
+++ src/Latest_Logics_and_Python_Ideas.py
@@ -0,0 +1,40 @@
+# 1. Usage of python decorators
+#Let us assume our function is:
+def somefunc(a,b):
+    output = a+b
+    return output
+
+import time
+def somefunc(a,b):
+    print("somefunc begins")
+    start_time = time.time()
+    output = a+b
+    print("somefunc ends in ",time.time()-start_time,"secs")
+    return output
+out = somefunc(4,5)
+
+#Output
+'''
+somefunc begins
+somefunc ends in  0.0 secs
+'''
+
+# Using decorators
+from functools import wraps
+def timer(func):
+    @wraps(func)
+    def wrapper(a,b):
+        print(f"{func.__name__!r} begins")
+        start_time = time.time()
+        result = func(a,b)
+        print(f"{func.__name__!r} ends in {time.time()-start_time} secs")
+        return result
+    return wrapper
+
+@timer
+def somefunc(a,b):
+    output = a+b
+    return output
+
+a = somefunc(4,5)
+
